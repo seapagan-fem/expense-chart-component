9253cdbf19d5739ce4bec20e1b04cef4675356d8
Grant Ramsay <seapagan@gmail.com>
2022-07-18 20:42:27 +0100

    hopefully fix json path

--- scripts/site.js
+++ scripts/site.js
@@ -3,7 +3,7 @@ console.log("Javascript Loaded");
 let spendData = [];
 
 const getData = () => {
-  fetch("../data.json")
+  fetch("/data.json")
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
