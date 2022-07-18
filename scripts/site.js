35387c9ab2dbdbc30f18e1befe0c760d8b4e8c03
Grant Ramsay <seapagan@gmail.com>
2022-07-18 20:33:15 +0100

    complete static styling
    now have the dynamic bars to work on

--- scripts/site.js
+++ scripts/site.js
@@ -1,1 +1,14 @@
 console.log("Javascript Loaded");
+
+let spendData = [];
+
+const getData = () => {
+  fetch("../data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      // in here we will populate the chart
+    });
+};
+
+getData();
