ab11088167e9586827478678ae8f404c3d325d89
Grant Ramsay <seapagan@gmail.com>
2022-07-19 11:33:00 +0100

    complete mobile view

--- scripts/site.js
+++ scripts/site.js
@@ -1,13 +1,49 @@
 console.log("Javascript Loaded");
 
-let spendData = [];
+const today = new Date();
+const weekday = today
+  .toLocaleDateString("default", { weekday: "short" })
+  .toLowerCase();
+
+console.log(weekday);
+
+const createBar = ({ day, amount }, maxSpend) => {
+  bar = document.createDocumentFragment();
+
+  barWrapper = document.createElement("div");
+  barWrapper.className = "bar-wrapper";
+
+  dayName = document.createElement("div");
+  dayName.className = "bar-axis";
+  dayName.innerText = day;
+
+  spendBar = document.createElement("div");
+  spendBar.className = "spend-bar";
+  if (day === weekday) {
+    spendBar.classList.add("today");
+  }
+
+  // we take 150px as being the largest bar, then normalize the other values to
+  // this.
+  spendBar.style.height = `${Math.round(150 * (amount / maxSpend))}px`;
+
+  barWrapper.append(spendBar);
+  barWrapper.append(dayName);
+
+  bar.append(barWrapper);
+
+  return bar;
+};
 
 const getData = () => {
   fetch("data.json")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      // in here we will populate the chart
+      const graph = document.querySelector(".bar-graph");
+      maxSpend = Math.max(...data.map((day) => day.amount));
+      data.map((day) => {
+        graph.append(createBar(day, maxSpend));
+      });
     });
 };
 
